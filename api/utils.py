8f5598acb2e661135b0c869610d7823ca61074a9
Eduardo <eduardo.pach@hotmail.com>
2023-01-10 22:17:22 -0300

    Image batch generator class

--- api/utils.py
+++ api/utils.py
@@ -21,6 +21,26 @@ def compute_embedding(
     embedding_type: str="visual",
     device: str="cpu"
 ) -> torch.Tensor:
+    """_summary_
+
+    Parameters
+    ----------
+    model : CLIPModel
+        _description_
+    processor : CLIPProcessor
+        _description_
+    batch : Union[list[Image.Image], list[str]]
+        _description_
+    embedding_type : str, optional
+        _description_, by default "visual"
+    device : str, optional
+        _description_, by default "cpu"
+
+    Returns
+    -------
+    torch.Tensor
+        _description_
+    """
     model.to(device)
     if embedding_type=="visual":
         processed_images = processor(images=batch, return_tensors="pt").to(device)
@@ -29,3 +49,42 @@ def compute_embedding(
         processed_text = processor(text=batch, return_tensors="pt").to(device)
         return model.get_text_features(**processed_text)
 
+
+class ImageBatchGenerator:
+    """  A generator class that takes a list of URLs (associated with images) and a batch size 
+    as arguments and yields batches of images when iterated over. 
+
+    Parameters
+    ----------
+    urls : list[str]
+        List of URLs
+    batch_size : int, optional
+        Number of URLs to yield in each batch, by default 32
+    """
+    def __init__(self, urls: list[str], batch_size: int=32) -> None:
+        self.urls = urls
+        self.batch_size = batch_size
+        self.current_index = 0
+
+    @staticmethod
+    def _load_image(self, url: str) -> Image.Image:
+        img_bytes = requests.get(url).content
+        return Image.open(BytesIO(img_bytes))
+
+    def __len__(self) -> int:
+        return self.batch_size
+
+    def __iter__(self) -> ImageBatchGenerator:
+        return self
+    
+    def __next__(self) -> list[Image.Image]:
+        if self.current_index >= len(self.urls):
+            raise StopIteration()
+        
+        start = self.current_index
+        end = min(start + self.batch_size, len(self.urls))
+        self.current_index = end
+        return [self._load_image(url) for url in self.urls[start:end]]
+
+
+    
