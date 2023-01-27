69331471c7618e1b80bf83430f82e551bbe38df4
Eduardo <eduardo.pach@hotmail.com>
2023-01-27 11:16:44 -0300

    Changed div config for image collection

--- app.py
+++ app.py
@@ -17,9 +17,17 @@ def main() -> None:
     model, processor = load_model()
     c1, c2 = st.columns(2)
     query = c1.text_input("Image Query", value="Two dogs playing")
-    k = c2.slider("Number of Images", min_value=1, max_value=20, step=1)
+    k = c2.slider("Number of Images", min_value=1, max_value=20, step=1, value=10)
     img_urls = search_image_from_text(model, processor, query, k)
-    clicked = clickable_images(img_urls, img_style={"margin": "5px", "height": "200px"})
+    clicked = clickable_images(
+        img_urls, 
+        img_style={"margin": "5px", "height": "200px"},
+        div_style={
+            "display": "flex",
+            "justify-content": "center",
+            "flex-wrap": "wrap",
+        }
+    )
 
 if __name__=="__main__":
     main()
