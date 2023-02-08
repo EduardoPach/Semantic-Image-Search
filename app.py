2cd67fb9652a26b75a36c7358c8cfc4f4aa8bb90
EduardoPach <eduardo.pach@hotmail.com>
2023-02-08 17:23:32 -0300

    using new function to search query

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-import torch
 import streamlit as st
 from st_clickable_images import clickable_images
-from transformers import CLIPModel, CLIPProcessor, CLIPVisionModel
+from transformers import CLIPModel, CLIPProcessor
 
-from api.utils import search_image_from_text
+from api.utils import search_image_from_query
 
 @st.experimental_singleton
 def load_model() -> tuple[CLIPModel, CLIPProcessor]:
@@ -18,7 +17,7 @@ def main() -> None:
     c1, c2 = st.columns(2)
     query = c1.text_input("Image Query", value="Two dogs playing")
     k = c2.slider("Number of Images", min_value=1, max_value=20, step=1, value=10)
-    img_urls = search_image_from_text(model, processor, query, k)
+    img_urls = search_image_from_query(model, processor, query, k)
     clicked = clickable_images(
         img_urls, 
         img_style={"margin": "5px", "height": "200px"},
