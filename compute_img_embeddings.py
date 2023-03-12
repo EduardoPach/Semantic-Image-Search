d75b7eee8a8290fc6fd51507685e13ecac267464
EduardoPach <eduardo.pach@hotmail.com>
2023-03-12 12:14:45 -0300

    Changes to add to existing pipeline

--- compute_img_embeddings.py
+++ compute_img_embeddings.py
@@ -14,7 +14,7 @@ from api import ImageBatchGenerator, SemanticSearcher
 
 def main(
     input_dir: str,
-    model_id: str="openai/clip-vit-large-patch14",
+    model_id: str="openai/clip-vit-base-patch32",
     batch_size: int=100
 ) -> None:
     """Precompute image embeddings and save them to disk with 
@@ -26,7 +26,7 @@ def main(
         A directory containing images to process
     model_id : str, optional
         Hugging Face model id from pretrained CLIP model to use
-        , by default "openai/clip-vit-large-patch14"
+        , by default "openai/clip-vit-large-patch14"        
     batch_size : int, optional
         Batch size to use while computing embeddings, by default 100
     """
@@ -38,7 +38,9 @@ def main(
     searcher = SemanticSearcher(model_id=model_id)
     embedding_dimension = searcher.model.visual_projection.out_features
 
-    img_list = os.listdir(input_dir)
+    dir_ = Path(input_dir)
+    img_list = list(dir_.glob("*"))
+    img_list = [str(img) for img in img_list]
     batch_generator = ImageBatchGenerator(img_list, batch_size)
 
     if os.path.exists(index_checkpoint):
@@ -56,7 +58,7 @@ def main(
         embedding /= np.linalg.norm(embedding, axis=1, keepdims=True)
         if not embeddings_temp_dir.exists():
             os.mkdir(embeddings_temp_dir)
-        name_prefix = embeddings_temp_dir / f"{batch_idx:05d}"
+        name_prefix = str(embeddings_temp_dir / f"{batch_idx:05d}")
         pd.DataFrame(img_path, columns=["path"]).to_parquet(f"{name_prefix}.parquet", index=False)
         np.save(f"{name_prefix}.npy", embedding)
 
@@ -72,10 +74,12 @@ def main(
     faiss.write_index(index, str(embeddings_dir / index_checkpoint))
 
     if os.path.exists(index_checkpoint):
-        url_file_path = embeddings_dir / (index_checkpoint.split(".")[0]+'.parquet')
-        curr_img_path = pd.read_csv(url_file_path)
+        curr_img_path = pd.read_csv(db_checkpoint)
         img_path = pd.concat([curr_img_path, img_path]).reset_index(drop=True)
 
-    img_path.to_csv(url_file_path, index=False)
+    img_path.to_parquet(str(embeddings_dir / db_checkpoint), index=False)
 
     shutil.rmtree(embeddings_temp_dir)
+
+if __name__ == "__main__":
+    main(input_dir="test_dir")
