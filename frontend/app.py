36db2d98759583f371b27627b99472ff9e730e67
EduardoPach <eduardo.pach@hotmail.com>
2023-04-08 18:40:43 -0300

    Now can input images to search

--- frontend/app.py
+++ frontend/app.py
@@ -1,14 +1,28 @@
 import streamlit as st
 from st_clickable_images import clickable_images
 
-from utils import send_request
+from utils import send_request, encode_image
 
 def main() -> None:
     st.set_page_config(layout="wide")
     st.title("Image Semantic Search - Unsplash")
-    c1, c2 = st.columns(2)
-    query = c1.text_input("Image Query", value="Two dogs playing")
-    k = c2.slider("Number of Images", min_value=1, max_value=20, step=1, value=10)
+
+    with st.sidebar.container():
+        c1, c2 = st.columns(2)
+        mode = c1.radio("Query Mode", ["Text", "Image"])
+        k = c2.slider("Number of Images", min_value=1, max_value=20, step=1, value=10)
+        if mode == "Text":
+            query = st.text_input("Query", value="Two dogs playing")
+        else:
+            query = None
+            uploaded_image = st.file_uploader("Query", type=["png", "jpg", "jpeg"])
+            if uploaded_image:
+                query = encode_image(uploaded_image.read()) 
+                st.image(uploaded_image, use_column_width=True, caption="Query Image")
+
+
+    if not query:
+        return 
     img_urls = send_request(query, k)
     clicked = clickable_images(
         img_urls, 
