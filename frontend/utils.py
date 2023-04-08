5f743540ce6f969a5ae7ad3ddc4213f88614d724
EduardoPach <eduardo.pach@hotmail.com>
2023-04-08 18:41:01 -0300

    New encode function to use in the frontend

--- frontend/utils.py
+++ frontend/utils.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+import base64
 import requests
+
+import streamlit as st
 
 def send_request(query: str, k: int) -> list[str]:
     """Send request to backend and return list of image URLs."""
@@ -13,3 +16,8 @@ def send_request(query: str, k: int) -> list[str]:
         payload = {"query": [query], "k": k}
         response = requests.post(url, json=payload)
     return response.json()["urls"][0]
+
+def encode_image(image_bytes: bytes) -> str:
+    """Encode image to base64."""
+    encoded_string = base64.b64encode(image_bytes).decode("utf-8")
+    return f"data:image/png;base64,{encoded_string}"
