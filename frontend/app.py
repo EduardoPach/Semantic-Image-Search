cbe65fe7fd0b1f91c49aea2cfdba811276faf2c4
EduardoPach <eduardo.pach@hotmail.com>
2023-04-08 19:10:21 -0300

    Now can refine search by clicking on images

--- frontend/app.py
+++ frontend/app.py
@@ -1,29 +1,19 @@
 import streamlit as st
 from st_clickable_images import clickable_images
 
-from utils import send_request, encode_image
+import utils
 
 def main() -> None:
     st.set_page_config(layout="wide")
     st.title("Image Semantic Search - Unsplash")
 
-    with st.sidebar.container():
-        c1, c2 = st.columns(2)
-        mode = c1.radio("Query Mode", ["Text", "Image"])
-        k = c2.slider("Number of Images", min_value=1, max_value=20, step=1, value=10)
-        if mode == "Text":
-            query = st.text_input("Query", value="Two dogs playing")
-        else:
-            query = None
-            uploaded_image = st.file_uploader("Query", type=["png", "jpg", "jpeg"])
-            if uploaded_image:
-                query = encode_image(uploaded_image.read()) 
-                st.image(uploaded_image, use_column_width=True, caption="Query Image")
-
+    query, k = utils.start_sidebar()
 
     if not query:
         return 
-    img_urls = send_request(query, k)
+    
+    st.subheader("Query Results")
+    img_urls = utils.send_request(query, k)
     clicked = clickable_images(
         img_urls, 
         img_style={"margin": "5px", "height": "200px"},
@@ -34,5 +24,20 @@ def main() -> None:
         }
     )
 
+    if clicked==-1:
+        return
+    
+    st.subheader("Refined Results")
+    refined_img_urls = utils.refined_search(img_urls[clicked], k)
+    clickable_images(
+        refined_img_urls, 
+        img_style={"margin": "5px", "height": "200px"},
+        div_style={
+            "display": "flex",
+            "justify-content": "center",
+            "flex-wrap": "wrap",
+        }
+    )
+    
 if __name__=="__main__":
     main()
--- frontend/utils.py
+++ frontend/utils.py
@@ -5,6 +5,21 @@ import requests
 
 import streamlit as st
 
+def fetch_image_bytes(url: str) -> bytes:
+    """This function fetches image bytes from url.
+
+    Parameters
+    ----------
+    url : str
+        URL of the image
+
+    Returns
+    -------
+    bytes
+        Fetched image bytes
+    """
+    return requests.get(url).content
+
 def send_request(query: str, k: int) -> list[str]:
     """Send request to backend and return list of image URLs."""
     try:
@@ -21,3 +36,40 @@ def encode_image(image_bytes: bytes) -> str:
     """Encode image to base64."""
     encoded_string = base64.b64encode(image_bytes).decode("utf-8")
     return f"data:image/png;base64,{encoded_string}"
+
+def start_sidebar() -> tuple[str, int]:
+    with st.sidebar.container():
+        c1, c2 = st.columns(2)
+        mode = c1.radio("Query Mode", ["Text", "Image"])
+        k = c2.slider("Number of Images", min_value=1, max_value=20, step=1, value=10)
+        if mode == "Text":
+            query = st.text_input("Query", value="Two dogs playing")
+        else:
+            query = None
+            uploaded_image = st.file_uploader("Query", type=["png", "jpg", "jpeg"])
+            if uploaded_image:
+                query = encode_image(uploaded_image.read()) 
+                st.image(uploaded_image, use_column_width=True, caption="Query Image")
+        
+        return query, k
+
+def refined_search(url: str, k) -> list[str]:
+    """Refined search using the clicked image.
+
+    Parameters
+    ----------
+    url : str
+        URL of the clicked image
+    k : _type_
+        Number of images to return
+
+    Returns
+    -------
+    list[str]
+        List of image URLs most similar to the clicked image
+    """
+    query = fetch_image_bytes(url)
+    query = encode_image(query)
+    img_urls = send_request(query, k)
+    return img_urls
+
